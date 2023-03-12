020bb13c11015a39aac1e7d63b3dc01b30f5f0d7
esckeyboard <70459924+esckeyboard@users.noreply.github.com>
2023-03-12 23:06:14 +0700

    Create
    config files for the bot

---
+++ config.py
@@ -0,0 +1,8 @@
+# set the ID of the server and channel to relay messages to
+server_id = 1234567890 # YOUR SERVER ID
+channel_id_curhat = 1234567890 # YOUR CHANNEL ID FOR RELAYED MESSAGES
+channel_id_ai = 1234567890 # YOUR CHANNEL ID FOR AI PROMPT
+
+token = 'YOUR TOKEN HERE'
+
+openai_key = 'YOUR KEY HERE'
